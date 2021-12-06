342cfae7c0a31f5497c2d3eb25d64cd9201765b8
LP <Louis-Philip@synthesis.co.za>
2021-12-06 15:11:33 +0200

    testing code again again again

--- amplify.tf
+++ amplify.tf
@@ -2,36 +2,40 @@ provider "aws" {
   region = "eu-west-1"
 }
 
-resource "aws_amplify_app" "personal-website" {
-  name       = "personal-website"
-  repository = "https://github.com/louisphilip/website.git"
-  enable_branch_auto_build = true
-  build_spec = <<-EOT
-    version: 0.1
-    frontend:
-      phases:
-        preBuild:
-          commands:
-            - npm install
-            - npm test -- --watchAll=false
-        build:
-          commands:
-            - npm run build
-      artifacts:
-        baseDirectory: build
-        files:
-          - '**/*'
-      cache:
-        paths:
-          - node_modules/**/*
-  EOT
-  # The default rewrites and redirects added by the Amplify Console.
-  custom_rule {
-    source = "/<*>"
-    status = "404"
-    target = "/index.html"
-  }
-  environment_variables = {
-    ENV = "dev"
-  }
+resource "aws_sns_topic" "user_updates" {
+  name = "user-updates-topic"
 }
+
+# resource "aws_amplify_app" "personal-website" {
+#   name       = "personal-website"
+#   repository = "https://github.com/louisphilip/website.git"
+#   enable_branch_auto_build = true
+#   build_spec = <<-EOT
+#     version: 0.1
+#     frontend:
+#       phases:
+#         preBuild:
+#           commands:
+#             - npm install
+#             - npm test -- --watchAll=false
+#         build:
+#           commands:
+#             - npm run build
+#       artifacts:
+#         baseDirectory: build
+#         files:
+#           - '**/*'
+#       cache:
+#         paths:
+#           - node_modules/**/*
+#   EOT
+#   # The default rewrites and redirects added by the Amplify Console.
+#   custom_rule {
+#     source = "/<*>"
+#     status = "404"
+#     target = "/index.html"
+#   }
+#   environment_variables = {
+#     ENV = "dev"
+#   }
+# }
