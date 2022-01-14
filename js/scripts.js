05fe633acb3679f2d223e26597328417fbc22259
LP <Louis-Philip@synthesis.co.za>
2022-01-14 09:17:14 +0200

    Trying the gaze template

--- js/scripts.js
+++ js/scripts.js
@@ -992,17 +992,6 @@
     });
   });
 
-  $.post($("#contact-us-form").attr('action'), JSON.stringify({
-    name: $("#contact-us-form input[name='name']").val(),
-    email: $("#contact-us-form input[name='email']").val(),
-    phone: $("#contact-us-form input[name='phone']").val(),
-    message: $("#contact-us-form textarea[name='message']").val(),
-    'g-recaptcha-response': $("#contact-us-form textarea[name='g-recaptcha-response']").val()
-}), function (data) {
-    $(".thanks").show();
-    $("#contact-us-form button").hide();
-}, 'json');
-
 
   /* Scroll to Top
   -------------------------------------------------------*/
